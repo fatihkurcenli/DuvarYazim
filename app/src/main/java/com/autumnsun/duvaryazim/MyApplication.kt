75ea839ad487ee593e6d219cb2cfd2362199aff8
Fatih <rizelifatih5353@gmail.com>
2021-12-07 18:42:02 +0300

    initial commit

---
+++ app/src/main/java/com/autumnsun/duvaryazim/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.autumnsun.duvaryazim
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+
+/*
+ Created by Fatih Kurcenli on 12/7/2021
+*/
+
+@AndroidEntryPoint
+class MyApplication : Application() {
+}
